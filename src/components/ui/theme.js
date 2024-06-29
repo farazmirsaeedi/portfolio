db6664246a0ba5d51b4ee790d94891e461676a7c
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-06-28 22:44:36 -0700

    change theme

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material/styles";
+import { blue, red } from "@mui/material/colors";
 
 //NOTE Create Custom Theme
 export const theme = createTheme({
@@ -14,5 +15,29 @@ export const theme = createTheme({
   },
   typography: {
     fontFamily: "vazir, roboto",
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "dashed" },
+          style: {
+            textTransform: "none",
+            border: `2px dashed ${blue[500]}`,
+          },
+        },
+        {
+          props: { variant: "dashed", color: "secondary" },
+          style: {
+            border: `4px dashed ${red[500]}`,
+          },
+        },
+      ],
+    },
   },
 });
