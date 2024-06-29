a64bb876ceb222c63fbfac7fefd7f13731234804
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-06-28 22:29:02 -0700

    refactoring app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import rtlPlugin from "stylis-plugin-rtl";
-import { CacheProvider } from "@emotion/react";
-import { HelmetProvider, Helmet } from "react-helmet-async";
-import createCache from "@emotion/cache";
-import { prefixer } from "stylis";
-
+import MainLayout from "./components/layouts/MainLayout";
 import Header from "./components/ui/Header";
-import { theme } from "./components/ui/theme";
-import "./App.css";
-
-//NOTE Create RTL Cache
-const cacheRTL = createCache({
-  key: "muirtl",
-  stylisPlugins: [prefixer, rtlPlugin],
-});
 
 function App() {
   return (
-    <CacheProvider value={cacheRTL}>
-      <ThemeProvider theme={theme}>
-        <HelmetProvider>
-          <Helmet>
-            <title>وب سایت شخصی یونس قربانی</title>
-          </Helmet>
-          <Header />
-          {[...new Array(200)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-            )
-            .join("\n")}
-        </HelmetProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <MainLayout>
+      <Header />
+    </MainLayout>
   );
 }
 
