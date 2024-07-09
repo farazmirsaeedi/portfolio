b541334f438fb2892b9497902919666425798772
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-09 13:17:40 -0700

    refactor and add swipeableviews

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 
 import { Typography } from "@mui/material";
+import SwipeableViews from "react-swipeable-views";
 
 import MainLayout from "../layouts/MainLayout";
 import { Sidebar } from "../components/sidebar";
-import ContentContainer from "./ContentContainer";
-import TabPanel from "../components/TabPanel";
+import PagesContainer from "./PagesContainer";
+import Page from "../pages/components/Page";
 import SidebarContainer from "./SidebarContainer";
 import MainContext from "../context";
+import { DrawerActionButton } from "../components/drawer";
+
+import { Home } from "../pages";
 
 function AppContainer() {
     const [pageNumber, setPageNumber] = useState(0);
     const [drawerOpen, setDrawerOpen] = useState(false);
 
     const handlePageNumber = (event, newPage) => {
-        console.log(newPage);
         setPageNumber(newPage);
     };
 
@@ -26,38 +29,57 @@ function AppContainer() {
                 <SidebarContainer>
                     <Sidebar />
                 </SidebarContainer>
-                <ContentContainer>
-                    <TabPanel pageNumber={pageNumber} index={0}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            صفحه اصلی
-                        </Typography>
-                    </TabPanel>
-                    <TabPanel pageNumber={pageNumber} index={1}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            درباره من
-                        </Typography>
-                    </TabPanel>
-                    <TabPanel pageNumber={pageNumber} index={2}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            رزومه من
-                        </Typography>
-                    </TabPanel>
-                    <TabPanel pageNumber={pageNumber} index={3}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            نمونه کارها
-                        </Typography>
-                    </TabPanel>
-                    <TabPanel pageNumber={pageNumber} index={4}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            نظرات دانشجویان
-                        </Typography>
-                    </TabPanel>
-                    <TabPanel pageNumber={pageNumber} index={5}>
-                        <Typography variant="h5" sx={{ textAlign: "center" }}>
-                            ارتباط با من
-                        </Typography>
-                    </TabPanel>
-                </ContentContainer>
+                <DrawerActionButton />
+                <PagesContainer>
+                    <SwipeableViews
+                        index={pageNumber}
+                        onChangeIndex={handlePageNumber}
+                    >
+                        <Page pageNumber={pageNumber} index={0}>
+                            <Home />
+                        </Page>
+                        <Page pageNumber={pageNumber} index={1}>
+                            <Typography
+                                variant="h5"
+                                sx={{ textAlign: "center" }}
+                            >
+                                درباره من
+                            </Typography>
+                        </Page>
+                        <Page pageNumber={pageNumber} index={2}>
+                            <Typography
+                                variant="h5"
+                                sx={{ textAlign: "center" }}
+                            >
+                                رزومه من
+                            </Typography>
+                        </Page>
+                        <Page pageNumber={pageNumber} index={3}>
+                            <Typography
+                                variant="h5"
+                                sx={{ textAlign: "center" }}
+                            >
+                                نمونه کارها
+                            </Typography>
+                        </Page>
+                        <Page pageNumber={pageNumber} index={4}>
+                            <Typography
+                                variant="h5"
+                                sx={{ textAlign: "center" }}
+                            >
+                                نظرات دانشجویان
+                            </Typography>
+                        </Page>
+                        <Page pageNumber={pageNumber} index={5}>
+                            <Typography
+                                variant="h5"
+                                sx={{ textAlign: "center" }}
+                            >
+                                ارتباط با من
+                            </Typography>
+                        </Page>
+                    </SwipeableViews>
+                </PagesContainer>
             </MainLayout>
         </MainContext.Provider>
     );
