f0e2585629bf1f13f9b16436b57d9aef561b01fd
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-09 23:18:59 -0700

    add drawer Action button

---
+++ src/components/drawer/DrawerActionButton.jsx
@@ -0,0 +1,30 @@
+import { Fab, Box } from "@mui/material";
+import { MenuRounded } from "@mui/icons-material";
+import { red } from "@mui/material/colors";
+
+const DrawerActionButton = () => {
+    return (
+        <Box
+            sx={{
+                display: {
+                    xs: "block",
+                    sm: "block",
+                    md: "none",
+                    lg: "none",
+                    xl: "none",
+                },
+            }}
+        >
+            <Fab
+                aria-label="Sidebar"
+                size="small"
+                onClick={() => setDrawerOpen(true)}
+                sx={{ backgroundColor: red[500], m: 2 }}
+            >
+                <MenuRounded />
+            </Fab>
+        </Box>
+    );
+};
+
+export default DrawerActionButton;
