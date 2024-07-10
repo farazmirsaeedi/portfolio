9af171775b92a5fdc0824fd115dcab5d604e4eda
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-09 23:23:09 -0700

    crate sidebarTabs

---
+++ src/components/sidebar/SidebarTabs.jsx
@@ -0,0 +1,135 @@
+import { Tabs, Tab } from "@mui/material";
+import {
+    HomeRounded,
+    FaceRounded,
+    TextSnippetRounded,
+    TerminalRounded,
+    MessageRounded,
+    ConnectWithoutContactRounded,
+} from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
+
+const SidebarTabs = ({ value, setDrawerOpen, handleChange }) => {
+    const tabProps = (index) => {
+        return {
+            id: `sidebar-tab-${index}`,
+            "aria-controls": `tabpanel-${index}`,
+        };
+    };
+
+    return (
+        <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            scrollButton="auto"
+            allowScrollButtonsMobile
+            value={value}
+            onChange={handleChange}
+        >
+            <Tab
+                label="صفحه اصلی"
+                icon={<HomeRounded />}
+                iconPosition="start"
+                sx={{
+                    backgroundColor: "tomato",
+                    backgroundColor: grey[800],
+                    borderRadius: 2,
+                    my: 0.5,
+                    mx: 1,
+                    "&.MuiTab-root": {
+                        minHeight: 50,
+                    },
+                }}
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(0)}
+            />
+
+            <Tab
+                label="درباره من"
+                icon={<FaceRounded />}
+                sx={{
+                    "&.MuiTab-root": {
+                        backgroundColor: "tomato",
+                        backgroundColor: grey[800],
+                        borderRadius: 2,
+                        my: 0.5,
+                        mx: 1,
+                        minHeight: 50,
+                    },
+                }}
+                iconPosition="start"
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(1)}
+            />
+            <Tab
+                label="رزومه من"
+                icon={<TextSnippetRounded />}
+                iconPosition="start"
+                sx={{
+                    "&.MuiTab-root": {
+                        backgroundColor: "tomato",
+                        backgroundColor: grey[800],
+                        borderRadius: 2,
+                        my: 0.5,
+                        mx: 1,
+                        minHeight: 50,
+                    },
+                }}
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(2)}
+            />
+            <Tab
+                label="نمونه کارها"
+                icon={<TerminalRounded />}
+                iconPosition="start"
+                sx={{
+                    "&.MuiTab-root": {
+                        backgroundColor: "tomato",
+                        backgroundColor: grey[800],
+                        borderRadius: 2,
+                        my: 0.5,
+                        mx: 1,
+                        minHeight: 50,
+                    },
+                }}
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(3)}
+            />
+            <Tab
+                label="نظرات دانشجویان"
+                icon={<MessageRounded />}
+                iconPosition="start"
+                sx={{
+                    "&.MuiTab-root": {
+                        backgroundColor: "tomato",
+                        backgroundColor: grey[800],
+                        borderRadius: 2,
+                        my: 0.5,
+                        mx: 1,
+                        minHeight: 50,
+                    },
+                }}
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(4)}
+            />
+            <Tab
+                label="ارتباط با من"
+                icon={<ConnectWithoutContactRounded />}
+                iconPosition="start"
+                sx={{
+                    "&.MuiTab-root": {
+                        backgroundColor: grey[800],
+                        borderRadius: 2,
+                        my: 0.5,
+                        mx: 1,
+                        minHeight: 50,
+                    },
+                }}
+                onClick={() => setDrawerOpen(false)}
+                {...tabProps(5)}
+            />
+        </Tabs>
+    );
+};
+
+export default SidebarTabs;
