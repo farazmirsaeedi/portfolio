d9077bdeb3bcb4e626110c6246373d260f9028b9
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-09 23:21:50 -0700

    refactor Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,60 +1,14 @@
-import { useState } from "react";
-
-import { Drawer, Fab, Box } from "@mui/material";
-import { red } from "@mui/material/colors";
-import { MenuRounded } from "@mui/icons-material";
-
-import DrawerContent from "../DrawerContent";
+import { SidebarContent } from "./";
+import { DrawerActionButton, SidebarDrawer } from "../drawer";
 
 const Sidebar = ({ value, handleChange }) => {
-    const [drawerOpen, setDrawerOpen] = useState(false);
-
     return (
         <>
-            <Box
-                sx={{
-                    display: {
-                        xs: "block",
-                        sm: "block",
-                        md: "none",
-                        lg: "none",
-                        xl: "none",
-                    },
-                }}
-            >
-                <Fab
-                    aria-label="Sidebar"
-                    size="small"
-                    onClick={() => setDrawerOpen(true)}
-                    sx={{ backgroundColor: red[500], m: 2 }}
-                >
-                    <MenuRounded />
-                </Fab>
-            </Box>
-            <DrawerContent value={value} handleChange={handleChange} />
-            <Drawer
-                open={drawerOpen}
-                variant="temporary"
-                onClose={() => setDrawerOpen(false)}
-                sx={{
-                    "& .MuiDrawer-paper": {
-                        width: 300,
-                    },
-                    display: {
-                        xs: "block",
-                        sm: "block",
-                        md: "none",
-                        lg: "none",
-                    },
-                }}
-            >
-                {/* Drawer */}
-                <DrawerContent
-                    value={value}
-                    handleChange={handleChange}
-                    setDrawerOpen={setDrawerOpen}
-                />
-            </Drawer>
+            <DrawerActionButton />
+
+            <SidebarContent value={value} handleChange={handleChange} />
+
+            <SidebarDrawer />
         </>
     );
 };
