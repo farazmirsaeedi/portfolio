2131d625725c2903b2b7edc73604b0b3be161e66
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-18 11:14:26 -0700

    set color for toggle theme

--- src/components/sidebar/SidebarTabs.jsx
+++ src/components/sidebar/SidebarTabs.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 
 import { Tabs, Tab } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
 
 import MainContext from "../../context";
@@ -10,6 +11,7 @@ const SidebarTabs = () => {
     const { pageNumber, handlePageNumber, setDrawerOpen } =
         useContext(MainContext);
 
+    const theme = useTheme();
     const data = tabsData();
 
     return (
@@ -19,8 +21,10 @@ const SidebarTabs = () => {
             scrollButton="auto"
             allowScrollButtonsMobile
             value={pageNumber}
-            indicatorColor="secondary"
-            textColor="secondary"
+            indicatorColor={
+                theme.palette.mode === "dark" ? "secondary" : "primary"
+            }
+            textColor={theme.palette.mode === "dark" ? "secondary" : "primary"}
             onChange={handlePageNumber}
         >
             {data.map((tab, index) => (
@@ -30,8 +34,11 @@ const SidebarTabs = () => {
                     icon={tab.icon}
                     iconPosition="start"
                     sx={{
-                        backgroundColor: "tomato",
-                        backgroundColor: grey[800],
+                        backgroundColor:
+                            theme.palette.mode === "dark"
+                                ? grey[800]
+                                : grey[500],
+                        color: "text.primary",
                         borderRadius: 2,
                         my: 0.5,
                         mx: 1,
