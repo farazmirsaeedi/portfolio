e1ba67b7ea76808e96c71cae8f2a9d84ce4cc7a9
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-07-18 11:22:19 -0700

    add tanha font

--- src/layouts/theme/index.js
+++ src/layouts/theme/index.js
@@ -1,8 +1,22 @@
 import { createTheme } from "@mui/material/styles";
-import { blue, red } from "@mui/material/colors";
 
-//NOTE Create Custom Theme
-export const theme = createTheme({
+export const lightTheme = createTheme({
+    direction: "rtl",
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#8be9fd",
+        },
+        secondary: {
+            main: "#bd93f9",
+        },
+    },
+    typography: {
+        fontFamily: "tanha, vazir, roboto",
+    },
+});
+
+export const darkTheme = createTheme({
     direction: "rtl",
     palette: {
         mode: "dark",
@@ -15,29 +29,5 @@ export const theme = createTheme({
     },
     typography: {
         fontFamily: "tanha, vazir, roboto",
-    },
-    components: {
-        MuiButtonBase: {
-            defaultProps: {
-                disableRipple: true,
-            },
-        },
-        MuiButton: {
-            variants: [
-                {
-                    props: { variant: "dashed" },
-                    style: {
-                        textTransform: "none",
-                        border: `2px dashed ${blue[500]}`,
-                    },
-                },
-                {
-                    props: { variant: "dashed", color: "secondary" },
-                    style: {
-                        border: `4px dashed ${red[500]}`,
-                    },
-                },
-            ],
-        },
     },
 });
